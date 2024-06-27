91aa6ce803942e63929b59d5f478440503069f60
Sanket Sutar <sanketsutar1906@gmail.com>
2024-06-27 14:07:36 +0530

    sidebar for the desktop view(halfway done persue for mobile view after this)

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,17 @@
+import MobileNav from "@/components/MobileNav";
+import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = {firstName:"Sanket", lastName: "S"};
   return (
-    <main>
-        SIDEBAR
-        {children}
+    <main className="flex h-screen w-full font-inter">
+        <Sidebar user={loggedIn} />
+          {children}
     </main>
   );
 }
