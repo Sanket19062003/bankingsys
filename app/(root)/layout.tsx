bf2c391a1c7c98aa03979b0eb1ca802a052be013
Sanket Sutar <sanketsutar1906@gmail.com>
2024-06-27 16:13:33 +0530

    added left side bar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -11,7 +11,18 @@ export default function RootLayout({
   return (
     <main className="flex h-screen w-full font-inter">
         <Sidebar user={loggedIn} />
+
+        <div className="flex size-full flex-col">
+          <div className="root-layout">
+            <Image src="/icons/logo.svg" width={30} height={30} alt="logo"/>
+            <div>
+                <MobileNav user={loggedIn}/>
+
+            </div>
+
+          </div>
           {children}
+        </div>
     </main>
   );
 }
